d20e3553e60168d212e51ff168b9ad56d6034fb3
santanumondal9000 <santanu.mondal@innoraft.com>
2025-04-02 11:07:23 +0530

    FRES-74: Add Assignment5.

---
+++ Assignment5/js/assignment5.js
@@ -0,0 +1,66 @@
+// Update the Full name
+function updateFullname() {
+	let firstName = document.getElementById("first_name").value;
+	let lastName = document.getElementById("last_name").value;
+	document.getElementById("full_name").value = firstName + " " + lastName;
+}
+// Validate Form
+function validateForm() {
+	let firstName = document.forms["my-form"]["first-name"].value;
+	let lastName = document.forms["my-form"]["last-name"].value;
+	let fileInput = document.getElementById("image-file");
+	let filePath = fileInput.value;
+	let allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
+	let namePattern = /^[A-Za-z]+$/;
+	let phoneNumber = document.forms["my-form"]["phone-number"].value.trim();
+	let email = document.getElementById("email").value.trim();
+	let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+	if (firstName.trim() === "") {
+		alert("First Name must be filled");
+		return false;
+	}
+	if (!namePattern.test(firstName)) {
+		alert("First name must contain only alphabets.");
+		return false;
+	}
+	if (lastName.trim() === "") {
+		alert("Last Name must be filled");
+		return false;
+	}
+	if (!namePattern.test(lastName)) {
+		alert("Last name must contain only alphabets.");
+		return false;
+	}
+	if (fileInput.files.length === 0) {
+		alert("Image File must be selected");
+		return false;
+	}
+	if (!allowedExtensions.test(filePath)) {
+		alert("File Type Should Be PNG, JPEG, JPG, or GIF");
+		fileInput.value = "";
+		return false;
+	}
+	if (phoneNumber === "") {
+		alert("Phone Number Must be Filled!")
+	}
+	if (phoneNumber.startsWith("+91")) {
+		phoneNumber = phoneNumber.slice(3);
+	}
+
+	if (isNaN(phoneNumber)) {
+		alert("Phone Number should contain only numbers!");
+		return false;
+	}
+
+	if (phoneNumber.length !== 10) {
+		alert("Phone Number must be exactly 10 digits!");
+		return false;
+	}
+	if (!emailPattern.test(email)) {
+		alert("Invalid email address!");
+		return false;
+	}
+
+	return true;
+}
