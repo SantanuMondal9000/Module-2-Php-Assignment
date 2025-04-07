69ea541af5b126fa643b3a73d7db03339a23da03
santanumondal9000 <santanu.mondal@innoraft.com>
2025-04-07 15:22:40 +0530

    FRES-74: Updated Validation.

--- Assignment2/php/action.php
+++ Assignment2/php/action.php
@@ -7,18 +7,17 @@ if (isset($_POST['submit'])) {
 }
 // Function Image Upload.
 
-function imageUpload(){
+function imageUpload()
+{
     $targetDir = __DIR__ . "/../image/";
     $targetFile = $targetDir . basename($_FILES["image-file"]["name"]);
-
     if (file_exists($targetFile)) {
         return "../image/" . basename($_FILES["image-file"]["name"]);
     } 
     else {
         if (move_uploaded_file($_FILES["image-file"]["tmp_name"], $targetFile)) {
             return "../image/" . basename($_FILES["image-file"]["name"]);
-        } 
-        else {
+        } else {
             return false;
         }
     }
@@ -30,12 +29,15 @@ function imageUpload(){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Assignment 1</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Assignment 2</title>
 </head>
 <body>
     <h1>Hello <?php echo $fullName ?></h1>
     <div class="image-container">
+    <div class="image-container">
     <img src="<?php echo $imagePath; ?>" alt="image" width="500" height="500">
+    </div>
 
     </div>
 </body>
