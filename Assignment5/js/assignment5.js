81607148632cd68ecd6705ab5df386760174f64e
santanumondal9000 <santanu.mondal@innoraft.com>
2025-04-08 10:54:54 +0530

    FRES-74: Keep image folder, ignore all image files inside.

--- Assignment5/js/assignment5.js
+++ Assignment5/js/assignment5.js
@@ -1,69 +1,167 @@
+
+// Variable.
+var firstNameValid = false;
+var lastNameValid = false;
+var nameLength = 30;
+var imageSelect = false;
+var phoneNumberValid = false;
+var emailValid = false;
+
 // Update the Full name.
 
 function updateFullname() {
 	let firstName = document.getElementById("first_name").value;
 	let lastName = document.getElementById("last_name").value;
 	document.getElementById("full_name").value = firstName + " " + lastName;
+
 }
 
 // Validate Form.
 
-function validateForm() {
-	let firstName = document.forms["my-form"]["first-name"].value;
+function validateName(id) {
+	updateFullname();
 	let lastName = document.forms["my-form"]["last-name"].value;
-	let fileInput = document.getElementById("image-file");
-	let filePath = fileInput.value;
-	let allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
 	let namePattern = /^[A-Za-z]+$/;
-	let phoneNumber = document.forms["my-form"]["phone-number"].value.trim();
-	let email = document.getElementById("email").value.trim();
+	if (id == "first-name") {
+		let firstName = document.forms["my-form"]["first-name"].value;
+		if (firstName.trim() == "") {
+			formStatus("#" + id + "-status", "First Name Should Be Filled!");
+			firstNameValid = false;
+		}
+		else if (!namePattern.test(firstName)) {
+			formStatus("#" + id + "-status", "first name must contain only alphabets.");
+			firstNameValid = false;
+
+		}
+		else if (firstName.length >= nameLength) {
+			formStatus("#" + id + "-status", "first name must below " + nameLength + " characters!");
+			firstNameValid = false;
+		}
+		else {
+			hideStatus("#" + id + "-status");
+			firstNameValid = true;
+		}
+	}
+	if (id == "last-name") {
+		if (lastName.trim() == "") {
+			formStatus("#" + id + "-status", "last Name Should Be Filled");
+			lastNameValid = false;
+		}
+		else if (!namePattern.test(lastName)) {
+			formStatus("#" + id + "-status", "Last name must contain only alphabets.");
+			lastNameValid = false;
+		}
+		else if (lastName.length >= nameLength) {
+			formStatus("#" + id + "-status", "last name must below " + nameLength + " characters!");
+			firstNameValid = false;
+		}
+		else {
+			hideStatus("#" + id + "-status");
+			lastNameValid = true;
+		}
+	}
+}
+// Function Image Valid.
+
+function imageValid(id) {
+	var fileInput = document.getElementById("image-file");
+	var filePath = fileInput.value;
+	var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
+	if ($("#" + id).get(0).files.length === 0) {
+		formStatus("#" + id + "-status", "image must be select!.");
+		imageSelect = false;
+	}
+	else if (!allowedExtensions.test(filePath)) {
+		formStatus("#" + id + "-status", "File Type Should Be PNG, JPEG, JPG, or GIF!.");
+		fileInput.value = "";
+		imageSelect = false;
+	}
+	else {
+		hideStatus("#" + id + "-status");
+		imageSelect = true;
+	}
+
+
+}
+// Validate Phone Number.
+
+function validatePhone(id) {
+	let phonePattern = /^[6-9]\d{9}$/;
+	let phoneNumber = $("#" + id).val().trim();
+	if (id == "phone-number") {
+		if (phoneNumber.startsWith("+91")) {
+			phoneNumber = phoneNumber.slice(3);
+		}
+
+		if (phoneNumber == "") {
+			formStatus("#" + id + "-status", "phone number must be filled!");
+			phoneNumberValid = false;
+
+		}
+		else if (isNaN(phoneNumber)) {
+			formStatus("#" + id + "-status", "Phone Number should contain only numbers!");
+			phoneNumberValid = false;
+		}
+		else if (phoneNumber.length !== 10) {
+			formStatus("#" + id + "-status", "Phone Number must be exactly 10 digits!");
+			phoneNumberValid = false;
+		}
+		else if (!phonePattern.test(phoneNumber)) {
+			formStatus("#" + id + "-status", "Phone Number can be start only 6,7,8,9");
+			phoneNumberValid = false;
+		}
+		else {
+			hideStatus("#" + id + "-status");
+			phoneNumberValid = true;
+		}
+	}
+}
+// Function Valid Email.
+
+function validEmail(id) {
+	let email = $("#" + id).val().trim();  // ✅ Get email input
 	let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-	if (firstName.trim() === "") {
-		alert("First Name must be filled");
-		return false;
-	}
-	if (!namePattern.test(firstName)) {
-		alert("First name must contain only alphabets.");
-		return false;
-	}
-	if (lastName.trim() === "") {
-		alert("Last Name must be filled");
-		return false;
-	}
-	if (!namePattern.test(lastName)) {
-		alert("Last name must contain only alphabets.");
-		return false;
-	}
-	if (fileInput.files.length === 0) {
-		alert("Image File must be selected");
-		return false;
-	}
-	if (!allowedExtensions.test(filePath)) {
-		alert("File Type Should Be PNG, JPEG, JPG, or GIF");
-		fileInput.value = "";
-		return false;
-	}
-	if (phoneNumber === "") {
-		alert("Phone Number Must be Filled!")
-	}
-	if (phoneNumber.startsWith("+91")) {
-		phoneNumber = phoneNumber.slice(3);
-	}
-
-	if (isNaN(phoneNumber)) {
-		alert("Phone Number should contain only numbers!");
-		return false;
-	}
-
-	if (phoneNumber.length !== 10) {
-		alert("Phone Number must be exactly 10 digits!");
-		return false;
-	}
 	if (!emailPattern.test(email)) {
-		alert("Invalid email address!");
+		formStatus("#" + id + "-status", "Invalid email address!");
+		emailValid = false;
+	} else {
+		hideStatus("#" + id + "-status");
+		emailValid = true;
+	}
+}
+
+// Fucntion Validate Form Submit.
+
+function formSubmit() {
+	validateName("first-name");
+	validateName("last-name");
+	imageValid("image-file");
+	validatePhone("phone-number");
+	validEmail("email")
+	console.log(emailValid);
+	if (firstNameValid && lastNameValid && imageSelect && phoneNumberValid && emailValid) {
+		return true;
+	}
+	else {
 		return false;
 	}
+}
 
-	return true;
+// Function Form Status.
+
+function formStatus(id, message) {
+	$(id).text(message).css({
+		"opacity": "1",
+		"text-transform": "capitalize"
+
+	});
+}
+
+// Function Hide Status.
+
+function hideStatus(id) {
+	$(id).css({
+		"opacity": "0"
+	});
 }
